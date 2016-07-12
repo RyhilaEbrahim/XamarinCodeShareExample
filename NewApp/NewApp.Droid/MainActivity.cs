350fa39700356666a6645bf3acc1d7e889c31982
Ryhila Ebrahim <ryhila15@gmail.com>
2016-07-12 15:05:36 +0200

    Intial commit

---
+++ NewApp/NewApp.Droid/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace NewApp.Droid
+{
+	[Activity (Label = "NewApp.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+	     SharedClass shared = new SharedClass();
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			var button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate {
+                button.Text = shared.IncreaseCount();
+			};
+		}
+	}
+}
+
+
---
+++ NewApp/NewApp/SharedClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NewApp
+{
+    public class SharedClass
+    {
+        public SharedClass()
+        {
+        }
+        private int Count { get; set; }
+
+        public string IncreaseCount()
+        {
+            Count++;
+            return $"You clicked {Count} time";
+        }
+    }
+}
+
